db36b139f4535bc08e2fbdc0d78b294e587b71b3
roblee32 <robin.lee@quickrelease.co.uk>
2023-09-29 08:21:47 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import messagebox
+
+# Create the main application window
+root = tk.Tk()
+root.withdraw()  # Hide the main window
+
+# Create a popup message box
+messagebox.showinfo("Hello", "Hello World")
+
